4acccf2a6329c1d461355a9049df920d7de45da5
systembugtj <albert_lee@hotmail.com>
2023-02-15 21:36:28 -0800

    feat: add split view

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,125 +1,146 @@
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <script setup lang="ts">
-import treeview from 'vue3-treeview'
-import { reactive, toRefs, toRef } from 'vue'
-import 'vue3-treeview/dist/style.css'
+import type { TreeProps } from 'ant-design-vue'
+import { ref, watch } from 'vue'
+import SplitView from 'vue-split-view'
 
-interface Node {
-  text: string
-  children?: string[]
-  state: {
-    isLoading: boolean
-  }
-}
-
-interface Config {
-  roots: string[]
-  leaves: string[]
-}
-
-interface Nodes {
-  [key: string]: Node
-}
-
-const config = reactive<Config>({
-  roots: ['id1', 'id2'],
-  leaves: ['fakeid']
-})
-
-const nodes = reactive<Nodes>({
-  id1: {
-    text: 'text1',
-    children: ['id11', 'id12'],
-    state: {
-      isLoading: false
-    }
-  },
-  id11: {
-    text: 'text11',
-    state: {
-      isLoading: false
-    }
-  },
-  id12: {
-    text: 'text12',
-    state: {
-      isLoading: false
-    }
-  },
-  id2: {
-    text: 'text2',
-    state: {
-      isLoading: false
-    }
-  }
-})
-
-const context = toRefs({
-  config,
-  nodes
-})
-
-function addServerNode(n: Node): void {
-  if (n.children && n.children.length > 0) return
-
-  // set node loading state to tree
-  n.state.isLoading = true
-
-  // fake server call
-  setTimeout(() => {
-    // create a fake node
-    const id = `${Date.now()}`
-    const newNode = {
-      text: `loaded from server`,
-      children: [],
-      state: {
-        isLoading: false
+const treeData: TreeProps['treeData'] = [
+  {
+    title: 'parent 1',
+    key: '0-0',
+    children: [
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+        disabled: true,
+        children: [
+          { title: 'leaf', key: '0-0-0-0', disableCheckbox: true },
+          { title: 'leaf', key: '0-0-0-1' }
+        ]
+      },
+      {
+        title: 'parent 1-1',
+        key: '0-0-1',
+        children: [{ key: '0-0-1-0', title: 'sss' }]
       }
-    }
+    ]
+  }
+]
 
-    // add the node to nodes
-    nodes[id] = newNode
-    // set children
-    n.children = [id]
-    // end loading
-    n.state.isLoading = false
-  }, 2000)
-}
+const expandedKeys = ref<string[]>(['0-0-0', '0-0-1'])
+const selectedKeys = ref<string[]>(['0-0-0', '0-0-1'])
+const checkedKeys = ref<string[]>(['0-0-0', '0-0-1'])
+const selectedKeys1 = ref<string[]>(['2'])
+//const selectedKeys2 = ref<string[]>(['1'])
+
+watch(expandedKeys, () => {
+  console.log('expandedKeys', expandedKeys)
+})
+watch(selectedKeys, () => {
+  console.log('selectedKeys', selectedKeys)
+})
+watch(checkedKeys, () => {
+  console.log('checkedKeys', checkedKeys)
+})
 </script>
 
 <template>
-  <v-card>
-    <v-layout>
-      <v-navigation-drawer>
-        <v-list>
-          <v-list-item title="Navigation drawer"></v-list-item>
-        </v-list>
-        <treeview :nodes="context.nodes" :config="context.config" @node-opened="addServerNode">
-          <template #loading-slot>
-            <div class="progress">
-              <div class="indeterminate"></div>
-            </div>
-          </template>
-        </treeview>
-      </v-navigation-drawer>
+  <a-layout>
+    <a-layout-header class="header">
+      <div class="logo" />
+      <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">nav 1</a-menu-item>
+        <a-menu-item key="2">nav 2</a-menu-item>
+        <a-menu-item key="3">nav 3</a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout>
+      <split-view>
+        <template #A>
+          <a-tree
+            v-model:expandedKeys="expandedKeys"
+            v-model:selectedKeys="selectedKeys"
+            v-model:checkedKeys="checkedKeys"
+            checkable
+            :tree-data="treeData"
+          >
+            <template #title="{ title, key }">
+              <span v-if="key === '0-0-1-0'" style="color: #1890ff">{{ title }}</span>
+              <template v-else>{{ title }}</template>
+            </template>
+          </a-tree>
+        </template>
 
-      <v-app-bar title="Application bar"></v-app-bar>
-
-      <v-main style="min-height: 300px">
-        <v-container>
-          <v-row>
-            <v-col cols="12">
-              <v-card>
-                <v-card-title>Content</v-card-title>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-    </v-layout>
-  </v-card>
+        <template #B>
+          <a-layout style="padding: 0 24px 24px">
+            <a-breadcrumb style="margin: 16px 0">
+              <a-breadcrumb-item>Home</a-breadcrumb-item>
+              <a-breadcrumb-item>List</a-breadcrumb-item>
+              <a-breadcrumb-item>App</a-breadcrumb-item>
+            </a-breadcrumb>
+            <a-layout-content
+              :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+            >
+              Content
+            </a-layout-content>
+          </a-layout>
+        </template>
+      </split-view>
+    </a-layout>
+    <a-layout-footer>Footer</a-layout-footer>
+  </a-layout>
 </template>
 
 <style lang="less">
-@import './assets/css/styles.less';
+#components-layout-demo-basic .code-box-demo {
+  text-align: center;
+}
+
+#components-layout-demo-basic .ant-layout-header,
+#components-layout-demo-basic .ant-layout-footer {
+  color: #fff;
+  background: #7dbcea;
+}
+
+[data-theme='dark'] #components-layout-demo-basic .ant-layout-header {
+  background: #6aa0c7;
+}
+
+[data-theme='dark'] #components-layout-demo-basic .ant-layout-footer {
+  background: #6aa0c7;
+}
+
+#components-layout-demo-basic .ant-layout-footer {
+  line-height: 1.5;
+}
+
+#components-layout-demo-basic .ant-layout-sider {
+  color: #fff;
+  line-height: 120px;
+  background: #3ba0e9;
+}
+
+[data-theme='dark'] #components-layout-demo-basic .ant-layout-sider {
+  background: #3499ec;
+}
+
+#components-layout-demo-basic .ant-layout-content {
+  min-height: 120px;
+  color: #fff;
+  line-height: 120px;
+  background: rgba(16, 142, 233, 1);
+}
+
+[data-theme='dark'] #components-layout-demo-basic .ant-layout-content {
+  background: #107bcb;
+}
+
+#components-layout-demo-basic > .code-box-demo > .ant-layout + .ant-layout {
+  margin-top: 48px;
+}
 </style>
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,15 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import './assets/css/styles.less'
 
-const vuetify = createVuetify({
-  components,
-  directives
-})
-
-createApp(App).use(vuetify).mount('#app')
+createApp(App).use(Antd).mount('#app')
