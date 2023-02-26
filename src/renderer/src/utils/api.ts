c2ab57ee1df4c317a6e23f8799b87ebba06f5819
systembugtj <albert_lee@hotmail.com>
2023-02-26 12:53:01 -0800

    fix: move to api.ts

--- src/renderer/src/utils/api.ts
+++ src/renderer/src/utils/api.ts
@@ -1,5 +1,18 @@
-import type { WatchConfig, WatchCallback } from "src/preload/index.d";
+import type {
+    WatchConfig,
+    WatchCallback,
+    ImportCallback,
+    DirectorySelection,
+} from "src/preload/index.d";
 
 export function startWatching(config: WatchConfig, callback: WatchCallback): void {
     window.api.startWatching(config, callback);
+}
+
+export function importPhotos(paths: string[], target: string, callback: ImportCallback): void {
+    window.api.importPhotos(paths, target, callback);
+}
+
+export function chooseDirectory(): Promise<DirectorySelection> {
+    return window.api.chooseDirectory();
 }
